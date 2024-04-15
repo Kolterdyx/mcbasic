3442bdc7f4451376d3b5961ecf539e1514826c34
kolterdyx <info@cirogarcia.dev>
2024-04-15 14:54:31 +0200

    safer string literals

--- internal/visitors/compiler/ops/register.go
+++ internal/visitors/compiler/ops/register.go
@@ -1,12 +1,18 @@
 package ops
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func (o *Op) Move(from string, to string) string {
 	return fmt.Sprintf("data modify storage %s:%s.%s set from storage %s:%s.%s\n", o.Namespace, VarPath, to, o.Namespace, VarPath, from)
 }
 
 func (o *Op) MoveConst(value string, to string) string {
+	if _, err := strconv.Atoi(value); err != nil {
+		value = strconv.Quote(value)
+	}
 	return fmt.Sprintf("data modify storage %s:%s.%s set value %s\n", o.Namespace, VarPath, to, value)
 }
 
