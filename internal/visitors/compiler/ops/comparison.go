adca7ac81032190f6bd2ed83e994d53103f4fc39
kolterdyx <info@cirogarcia.dev>
2024-04-18 03:06:42 +0200

    fix string comparison

--- internal/visitors/compiler/ops/comparison.go
+++ internal/visitors/compiler/ops/comparison.go
@@ -38,52 +38,6 @@ func (o *Op) NeqNumbers(ra string, rb string, rx string) string {
 	return o.CompNumbers("=", false, ra, rb, rx)
 }
 
-func (o *Op) EqStrings(ra string, rb string, rx string) string {
-	cmd := ""
-	// Two strings are equal if they are the same length and each character is the same.
-	// First we check if the strings are the same length
-	ralen := ra + "len"
-	rblen := rb + "len"
-	cmd += o.MoveConst("0", rx)
-	cmd += o.LenString(ra, ralen)
-	cmd += o.LenString(rb, rblen)
-	cmd += o.MoveScore(ralen, ralen)
-	cmd += o.MoveScore(rblen, rblen)
-	cmd += o.ExecCond(
-		fmt.Sprintf("score %s %s = %s %s", ralen, o.Namespace, rblen, o.Namespace),
-		false,
-		o.MoveConst("0", rx),
-	)
-	// If the strings are the same length, we compare the string values
-	// The easiest way to do this with MC commands is to check the success of a data command trying to overwrite the value of a string
-	// If the data command fails, the strings are equal
-	cmd += o.MoveScore(rx, rx)
-
-	stringComparison := ""
-	tmpReg := "__tmp__"
-	stringComparison += o.Move(ra, tmpReg)
-	stringComparison += fmt.Sprintf(
-		"execute store success storage %s:%s %s int 1 run data modify storage %s:%s %s set from storage %s:%s %s\n",
-		o.Namespace, VarPath, rx,
-		o.Namespace, VarPath, tmpReg,
-		o.Namespace, VarPath, rb)
-	condReg := rx + "__cond__"
-	cmd += o.MoveScore(rx, condReg)
-	cmd += o.ExecCond(
-		fmt.Sprintf("score %s %s matches 1", condReg, o.Namespace),
-		true,
-		stringComparison,
-	)
-	return cmd
-}
-
-func (o *Op) NeqStrings(ra string, rb string, rx string) string {
-	cmd := ""
-	cmd += o.EqStrings(ra, rb, rx)
-	cmd += o.NegateNumber(rx)
-	return cmd
-}
-
 func (o *Op) GtNumbers(ra string, rb string, rx string) string {
 	cmd := ""
 	cmd += o.CompNumbers(">", true, ra, rb, rx)
@@ -102,5 +56,52 @@ func (o *Op) LtNumbers(ra string, rb string, rx string) string {
 func (o *Op) LteNumbers(ra string, rb string, rx string) string {
 	cmd := ""
 	cmd += o.CompNumbers("<=", true, ra, rb, rx)
+	return cmd
+}
+
+func (o *Op) EqStrings(ra string, rb string, rx string) string {
+	cmd := ""
+	// Two strings are equal if they are the same length and each character is the same.
+	// First we check if the strings are the same length
+	ralen := ra + "len"
+	rblen := rb + "len"
+	cmd += o.MoveConst("1", rx)
+	cmd += o.LenString(ra, ralen)
+	cmd += o.LenString(rb, rblen)
+	cmd += o.MoveScore(ralen, ralen)
+	cmd += o.MoveScore(rblen, rblen)
+	cmd += o.ExecCond(
+		fmt.Sprintf("score %s %s = %s %s", ralen, o.Namespace, rblen, o.Namespace),
+		false,
+		o.MoveConst("0", rx),
+	)
+	// If the strings are the same length, we compare the string values
+	// The easiest way to do this with MC commands is to check the success of a data command trying to overwrite the value of a string
+	// If the data command fails, the strings are equal
+
+	condReg := rx + "__cond__"
+	cmd += o.MoveScore(rx, condReg)
+
+	stringComparison := ""
+	tmpReg := "__tmp__"
+	stringComparison += o.Move(ra, tmpReg)
+	stringComparison += fmt.Sprintf(
+		"execute store success storage %s:%s %s int 1 run data modify storage %s:%s %s set from storage %s:%s %s\n",
+		o.Namespace, VarPath, rx,
+		o.Namespace, VarPath, tmpReg,
+		o.Namespace, VarPath, rb)
+	cmd += o.ExecCond(
+		fmt.Sprintf("score %s %s matches 1", condReg, o.Namespace),
+		true,
+		stringComparison,
+	)
+	cmd += o.NegateNumber(rx)
+	return cmd
+}
+
+func (o *Op) NeqStrings(ra string, rb string, rx string) string {
+	cmd := ""
+	cmd += o.EqStrings(ra, rb, rx)
+	cmd += o.NegateNumber(rx)
 	return cmd
 }
