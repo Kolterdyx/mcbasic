c288d5236e36cc4307ef56d20218912888da1d07
Ciro García Belmonte <info@cirogarcia.dev>
2024-07-01 23:35:45 +0200

    Binary expression validation

--- src/commonMain/kotlin/me/kolterdyx/compiler/expression/BinaryExpression.kt
+++ src/commonMain/kotlin/me/kolterdyx/compiler/expression/BinaryExpression.kt
@@ -1,17 +1,119 @@
 package me.kolterdyx.compiler.expression
 
 import me.kolterdyx.compiler.Token
+import me.kolterdyx.compiler.TokenType
 import me.kolterdyx.compiler.ValueType
 import me.kolterdyx.compiler.ast.ExpressionVisitor
+import me.kolterdyx.compiler.exception.ParseException
 
-fun checkCompatibility(left: ValueType, right: ValueType): ValueType {
+object Constants {
+    val ValidIntRightHandTypes = setOf(ValueType.INT, ValueType.FLOAT)
+    val ValidIntOperators = mapOf(
+        ValueType.INT to setOf(
+            TokenType.PLUS,
+            TokenType.MINUS,
+            TokenType.STAR,
+            TokenType.SLASH,
+            TokenType.PERCENT,
+            TokenType.EQUAL_EQUAL,
+            TokenType.BANG_EQUAL,
+            TokenType.GREATER,
+            TokenType.GREATER_EQUAL,
+            TokenType.LESS,
+            TokenType.LESS_EQUAL
+        ),
+        ValueType.FLOAT to setOf(
+            TokenType.PLUS,
+            TokenType.MINUS,
+            TokenType.STAR,
+            TokenType.SLASH,
+            TokenType.PERCENT,
+            TokenType.EQUAL_EQUAL,
+            TokenType.BANG_EQUAL,
+            TokenType.GREATER,
+            TokenType.GREATER_EQUAL,
+            TokenType.LESS,
+            TokenType.LESS_EQUAL
+        ),
+    )
+
+    val ValidFloatRightHandTypes = setOf(ValueType.INT, ValueType.FLOAT)
+    val ValidFloatOperators = mapOf(
+        ValueType.INT to setOf(
+            TokenType.PLUS,
+            TokenType.MINUS,
+            TokenType.STAR,
+            TokenType.SLASH,
+            TokenType.PERCENT,
+            TokenType.EQUAL_EQUAL,
+            TokenType.BANG_EQUAL,
+            TokenType.GREATER,
+            TokenType.GREATER_EQUAL,
+            TokenType.LESS,
+            TokenType.LESS_EQUAL
+        ),
+        ValueType.FLOAT to setOf(
+            TokenType.PLUS,
+            TokenType.MINUS,
+            TokenType.STAR,
+            TokenType.SLASH,
+            TokenType.PERCENT,
+            TokenType.EQUAL_EQUAL,
+            TokenType.BANG_EQUAL,
+            TokenType.GREATER,
+            TokenType.GREATER_EQUAL,
+            TokenType.LESS,
+            TokenType.LESS_EQUAL
+        ),
+    )
+
+    val ValidStringRightHandTypes = setOf(
+        ValueType.STRING,
+        ValueType.INT,
+        ValueType.FLOAT,
+        ValueType.BOOLEAN
+    )
+    val ValidStringOperators = mapOf(
+        ValueType.STRING to setOf(
+            TokenType.PLUS,
+            TokenType.EQUAL_EQUAL,
+            TokenType.BANG_EQUAL
+        ),
+        ValueType.INT to setOf(
+            TokenType.PLUS,
+        ),
+        ValueType.FLOAT to setOf(
+            TokenType.PLUS,
+        ),
+        ValueType.BOOLEAN to setOf(
+            TokenType.PLUS,
+        )
+    )
+
+    val ValidBooleanRightHandTypes = setOf(ValueType.BOOLEAN)
+    val ValidBooleanOperators = mapOf(
+        ValueType.BOOLEAN to setOf(
+            TokenType.AND,
+            TokenType.OR,
+            TokenType.EQUAL_EQUAL,
+            TokenType.BANG_EQUAL
+        )
+    )
+}
+
+fun checkCompatibility(right: ValueType, operator: Token, validTypes: Set<ValueType>, validOperators: Map<ValueType, Set<TokenType>>): Boolean {
+    return validTypes.contains(right) && validOperators[right]!!.contains(operator.type)
+}
+
+fun checkCompatibility(left: ValueType, right: ValueType, operator: Token): ValueType {
     val isCompatible = when (left) {
-        ValueType.INT -> right == ValueType.INT
-        ValueType.STRING -> right == ValueType.STRING || right == ValueType.INT || right == ValueType.FLOAT
-        ValueType.FLOAT -> right == ValueType.FLOAT
+        ValueType.INT -> checkCompatibility(right, operator, Constants.ValidIntRightHandTypes, Constants.ValidIntOperators)
+        ValueType.FLOAT -> checkCompatibility(right, operator, Constants.ValidFloatRightHandTypes, Constants.ValidFloatOperators)
+        ValueType.STRING -> checkCompatibility(right, operator, Constants.ValidStringRightHandTypes, Constants.ValidStringOperators)
+        ValueType.BOOLEAN -> checkCompatibility(right, operator, Constants.ValidBooleanRightHandTypes, Constants.ValidBooleanOperators)
     }
     if (!isCompatible) {
-        throw Exception("Incompatible types: $left and $right")
+        throw ParseException(operator, "Incompatible types: $left and $right")
     }
     return left
 }
@@ -20,7 +122,7 @@ class BinaryExpression(
     val left: Expression,
     val operator: Token,
     val right: Expression
-) : Expression(checkCompatibility(left.valueType, right.valueType)) {
+) : Expression(checkCompatibility(left.valueType, right.valueType, operator)) {
     override fun <R> accept(visitor: ExpressionVisitor<R>): R {
         return visitor.visitBinary(this)
     }
