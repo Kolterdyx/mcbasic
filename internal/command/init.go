dd56bbec347baf56d5b1ffabe8a022a3411f7423
Ciro García Belmonte <info@cirogarcia.dev>
2025-04-04 18:07:20 +0200

    message

--- internal/command/init.go
+++ internal/command/init.go
@@ -3,11 +3,5 @@ package command
 import "github.com/urfave/cli/v3"
 
 func Init(cmd *cli.Command) error {
-
-	name := cmd.String("name")
-	if name == "" {
-
-	}
-
-	return nil
+	return cli.Exit("Sorry, the init command is not implemented yet :(", 1)
 }
