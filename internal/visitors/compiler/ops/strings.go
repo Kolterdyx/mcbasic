82383fda0f8fd531e90abcb0708f71646a21895c
Ciro García Belmonte <info@cirogarcia.dev>
2025-04-14 06:04:39 +0200

    fix concatenation

--- internal/visitors/compiler/ops/strings.go
+++ internal/visitors/compiler/ops/strings.go
@@ -4,8 +4,8 @@ import "fmt"
 
 func (o *Op) Concat(var1, var2, result string) string {
 	cmd := ""
-	cmd += o.LoadArgConst("internal/concat", "res", RET)
-	cmd += o.LoadArgConst("internal/concat", "storage", fmt.Sprintf("%s:data %s", o.Namespace, VarPath))
+	cmd += o.LoadArgConst("internal/concat", "res", fmt.Sprintf("%s.%s", VarPath, RET))
+	cmd += o.LoadArgConst("internal/concat", "storage", fmt.Sprintf("%s:data", o.Namespace))
 	cmd += o.LoadArg("internal/concat", "a", var1)
 	cmd += o.LoadArg("internal/concat", "b", var2)
 	cmd += o.Call("mcb:internal/concat", result)
