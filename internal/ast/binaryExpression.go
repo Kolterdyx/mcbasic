ef34b2579e713b98b92c33906a189551349694e3
Ciro García Belmonte <info@cirogarcia.dev>
2025-05-14 13:16:25 +0200

    remove unused code

--- internal/ast/binaryExpression.go
+++ internal/ast/binaryExpression.go
@@ -26,29 +26,3 @@ func (b BinaryExpr) ToString() string {
 func (b BinaryExpr) GetSourceLocation() interfaces.SourceLocation {
 	return b.Left.GetSourceLocation()
 }
-
-//
-//func (b BinaryExpr) Validate() error {
-//	if b.Left == nil {
-//		return fmt.Errorf("BinaryExpr expression must have a left hand expression")
-//	}
-//	if b.Right == nil {
-//		return fmt.Errorf("BinaryExpr expression must have a right hand expression")
-//	}
-//	switch b.Left.ReturnType() {
-//	case types.IntType:
-//		switch b.Right.ReturnType() {
-//		case types.IntType:
-//		default:
-//			return fmt.Errorf("invalid righ hand time for type 'int' and operator '%s': %s", b.Operator.Lexeme, b.Right.ReturnType().ToString())
-//		}
-//	case types.StringType:
-//	case types.DoubleType:
-//		switch b.Right.ReturnType() {
-//		case types.DoubleType:
-//		default:
-//			return fmt.Errorf("invalid righ hand time for type 'double' and operator '%s': %s", b.Operator.Lexeme, b.Right.ReturnType().ToString())
-//		}
-//	}
-//	return nil
-//}
